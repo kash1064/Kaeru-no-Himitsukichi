d13c0d3f517f3487ee56a91541c25d9596ec30c0
kash1064 <kaship24@gmail.com>
2022-05-28 17:56:20 +0900

    Updates

--- sw.js
+++ sw.js
@@ -42,14 +42,14 @@ self.__precacheManifest = [
     "url": "532a2f07-92db97c0addf07d5cb73.js"
   },
   {
-    "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-3610fe15249f2c1afa93.js"
+    "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-707fd2f78426c4ff3c33.js"
   },
   {
     "url": "app-bcba0182970e9af19752.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ed4da128f69aec19f57e115afb8ffb61"
+    "revision": "1852567ed8af7b2343fed201fe0f46c4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-fd4fb51a6fac1c18bdde.js"
